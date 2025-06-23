39a34be098573b6f441f967827f339ea0c5e65e1
happYness7 <canweihong777@gmail.com>
2025-06-23 11:22:54 +0800

    博客最终版1

--- djangoAdmin/settings.py
+++ djangoAdmin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import datetime
 import boto3
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-21-4*vb6zk=emf&qk#b6qtgf-t=-3zr&_dqt+faz8r8)3xsit)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
